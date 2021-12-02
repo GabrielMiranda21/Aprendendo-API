853bf39c97be87d1b0c829918eb4bc5da6cb6e05
GabrielMiranda21 <gabrielmirand204@gmail.com>
2021-12-01 21:55:46 -0300

    Adicionando seeders e model através do sequelize, testando requisição no postman com rota

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.send("Hello World")
-})
+const TaskController = require('../controllers/TaskController');
+
+router.get('/', TaskController.index);
 
 module.exports = router;
